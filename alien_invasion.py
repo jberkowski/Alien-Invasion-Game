0b947480f08856ef27c34824df30149d74433667
Drugobrody <jakub.berkowski@gmail.com>
2022-09-29 15:31:45 +0200

    Added "_update_aliens" method and put it in the main loop.

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _create_fleet(self):
@@ -71,6 +72,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the postition of all aliens in the fleet."""
+        self.aliens.update()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
