3bd531126874704427bec7359aecc512cea000e2
Drugobrody <jakub.berkowski@gmail.com>
2022-10-11 11:38:18 +0200

    Play button is active during break.

--- alien_invasion.py
+++ alien_invasion.py
@@ -139,6 +139,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _update_aliens(self):
         """
@@ -183,6 +184,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
